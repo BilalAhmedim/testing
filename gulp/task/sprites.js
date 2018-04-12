a7693c8d32067658766b38df614dcd1562cc50ec
= <=>
2018-04-13 01:11:07 +0530

    Automated Genrated Sprites

---
+++ gulp/task/sprites.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp'),
+svgSprite = require('gulp-svg-sprite');
+
+var config = {
+  mode: {
+    css: {
+      render: {
+        pcss: {
+          template: './gulp/templates/sprites.pcss'
+        }
+      }
+    }
+  }
+}
+
+gulp.task('createSprite', function(){
+  return gulp.src('./app/assets/images/icons/**/*.svg')
+  .pipe(svgSprite(config))
+    .pipe(gulp.dest('./app/temp/sprites'));
+});
