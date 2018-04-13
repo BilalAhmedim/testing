243899d7e3a4201f004d103ce6b8e7a578692a24
= <=>
2018-04-13 22:01:31 +0530

    Complete automatic sprite generation.

--- gulp/task/sprites.js
+++ gulp/task/sprites.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp'),
-svgSprite = require('gulp-svg-sprite');
+svgSprite = require('gulp-svg-sprite'),
+rename = require('gulp-rename'),
+del = require('del');
 
 var config = {
   mode: {
     css: {
+      sprite: 'sprite.svg',
       render: {
         pcss: {
           template: './gulp/templates/sprites.pcss'
@@ -13,8 +16,29 @@ var config = {
   }
 }
 
-gulp.task('createSprite', function(){
+gulp.task('beginClean', function(){
+  return del(['./app/temp/sprites','./app/assets/images/sprites']);
+});
+
+gulp.task('createSprite', ['beginClean'], function(){
   return gulp.src('./app/assets/images/icons/**/*.svg')
   .pipe(svgSprite(config))
     .pipe(gulp.dest('./app/temp/sprites'));
 });
+
+gulp.task('copySpriteGraphic', ['createSprite'] ,function(){
+  return gulp.src('./app/temp/sprites/css/**/*.svg')
+    .pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+gulp.task('copySpritesCss', ['createSprite'] , function(){
+  return gulp.src('./app/temp/sprites/css/*.pcss')
+  .pipe(rename('_sprites.pcss'))
+  .pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+gulp.task('endClean', ['copySpriteGraphic', 'copySpritesCss'], function(){
+  return del('./app/temp/sprites');
+});
+
+gulp.task('icons',['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpritesCss', 'endClean']);
